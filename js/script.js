56484b91f4c4884dfddbc186428b9cb5348705ae
Anquilis <realanquilis@gmail.com>
2019-10-05 01:30:04 +0000

    javascript changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 $( "document" ).ready(function() {
+    var vowel =["a","e","i","o","u"];
 function pigLatinizer(string){
     string = $(".input").val();
-    $("#output").text(string+"ay");
+    string = txt.split("");
+    string.forEach(function(letter){
+        var letter = string.charAt(string.index[letter]);
+        if (letter == vowel){
+            string.replaceFirst(letter, "");
+            string = string + letter;
+        } else if (letter ==! vowel){
+            string = string + "ay";
+        }
+    });
+    $("#output").text(string);
 }
 
 $("#button").click(function() {
+    console.log("Hello world!");
     pigLatinizer();
 });
 
