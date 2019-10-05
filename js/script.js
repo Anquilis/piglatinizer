ad9a10ae597968c44700652e7a7a7b2350d1969a
Anquilis <realanquilis@gmail.com>
2019-10-05 18:37:09 +0000

    Js changes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 $( "document" ).ready(function() {
-    var vowel= ["a","e","i","o","u"];
+    var vowels= ["a","e","i","o","u"];
     function pigLatinizer(word){
         var word = $(".input").val();
         word = word.split("");
-        word.forEach(function(letter){
-            if (letter.equals(vowel)){
-                var letter = word.split(letter);
-                word = word.concat(letter);
-            } else {
-                word = word.concat("ay");
-            }
-        });
-        $("#output").text(word);
+        letter = word.slice(0,1);
+        word = word.slice(1);
+        word = word.concat(letter);
+        newWord = word.join('');
+        pigLatinWord = newWord.concat("ay");
+        $("#output").text(pigLatinWord);
     }
 
     $("#button").click(function() {
