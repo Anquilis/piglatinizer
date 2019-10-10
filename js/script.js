ec6259a80cd6b586de825b0bebe2df87285fbd45
Anquilis <realanquilis@gmail.com>
2019-10-10 00:12:21 +0000

    more JS changes... again

--- js/script.js
+++ js/script.js
@@ -1,23 +1,13 @@
 $( "document" ).ready(function() {
     var vowels= ["a","e","i","o","u"];
-    function pigLatinizer(word){
-        var word = $(".input").val();
-        word = word.split("");
-        // word.forEach(function(letter){
-        //     vowels.forEach(function(vowel){
-        //         if(letter==!vowel){
-        //             //take letter out of word
-        //             word.shift(letter);
-
-        //             //place letter into the end of word
-        //             word.push(letter);
-
-        //         }else if(letter === vowel){
-        //             stop();
-        //         }
-        //     });
-        // });
-        newWord = word.join('');
+    function pigLatinizer(){
+        var input = $(".input").val();
+        input = input.toLowerCase();
+        // var wordArr = input.split("");
+        var firstLetter = input.slice(0,1);
+        var restOfWord = input.slice(1,-1);
+        var newWord = restOfWord+firstLetter;
+        // newWord = wordArr.join('');
         pigLatinWord = newWord.concat("ay");
         $("#output").text(pigLatinWord);
     }
@@ -25,6 +15,5 @@ $( "document" ).ready(function() {
     $("#button").click(function() {
         pigLatinizer();
     });
-
 
 });
