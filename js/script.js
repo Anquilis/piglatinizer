68a5ddca5d2969aa977bb058e51a79ad47ea44ec
Anquilis <realanquilis@gmail.com>
2019-10-11 17:53:07 +0000

    JS changes

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@ $( "document" ).ready(function() {
     function pigLatinizer(){
         var input = $(".input").val();
         input = input.toLowerCase();
-        // var wordArr = input.split("");
-        var firstLetter = input.slice(0,1);
+
         var restOfWord = input.slice(1,-1);
+        var firstLetter = input.slice(0,1);
+
         var newWord = restOfWord+firstLetter;
-        // newWord = wordArr.join('');
-        pigLatinWord = newWord.concat("ay");
+        var pigLatinWord = newWord.concat("ay");
         $("#output").text(pigLatinWord);
     }
+
 
     $("#button").click(function() {
         pigLatinizer();
