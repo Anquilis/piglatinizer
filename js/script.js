dfdf0b59968b47d5084e86074c3dbd5daa682bd7
Anquilis <realanquilis@gmail.com>
2019-10-14 19:47:10 +0000

    Updated JS

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ $( "document" ).ready(function() {
 
         inputArr = input.split(" ");
         inputArr.forEach(function(word){
-                if(vowels.includes(word.charAt[0]))
-                var pigLatinWord = word.concat("way");
-                else {
-            for (i = 0; i < word.length; i++){
-                var restOfWord = inputArr.slice(i,word.length);
-                var firstLetter = inputArr.slice(0,i);
+                if(vowels.includes(word.charAt[0])){
+                    var pigLatinWord = word.concat("way");
+                } else {
+                    var firstLetter = inputArr.slice(0,1);
+                    var restOfWord = inputArr.slice(1,word.length);
+                    
+                    var newWord = restOfWord+firstLetter;
+                    var pigLatinWord = newWord.concat("ay");
                 }
-            }
-        });
-        var newWord = restOfWord+firstLetter;
+            });
 
         $("#output").text(pigLatinWord);
     }
