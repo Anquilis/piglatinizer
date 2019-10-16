2efa8eb2c710f32c3481f3b98585fe966835c64b
Anquilis <realanquilis@gmail.com>
2019-10-16 00:42:58 +0000

    The JavaScript is finally working! However, the code is redundant at times and only checks the first two characters

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
 $( "document" ).ready(function() {
-    var vowels= ["a","e","i","o","u"];
+    var vowels= ["a","e","i","o","u","A","E","I","O","U"];
+    var pigLatinArr = [];
     function pigLatinizer(){
         var input = $(".input").val();
-        input = input.toLowerCase();
-
-        inputArr = input.split(" ");
+        var inputArr = input.split(" ");
         inputArr.forEach(function(word){
-                if(vowels.includes(word.charAt[0])){
-                    var pigLatinWord = word.concat("way");
-                } else {
-                    var firstLetter = inputArr.slice(0,1);
-                    var restOfWord = inputArr.slice(1,word.length);
-                    
-                    var newWord = restOfWord+firstLetter;
-                    var pigLatinWord = newWord.concat("ay");
-                }
-            });
+            if(vowels.includes(word.charAt(0))){
+                var vowelPigLatin = word.concat("way");
+                pigLatinArr.push(vowelPigLatin);
+            } else{
+                //if it isn’t , take the letter out of the word
+                var firstLetter = word.slice(0,1);
+                var restOfWord = word.slice(1,word.length);
+		        //put it at the end of the word
+                var newWord = restOfWord+firstLetter;
 
-        $("#output").text(pigLatinWord);
+		        //repeat until the first character is a vowel
+                //put "ay" at the end of the word
+                if (vowels.includes(newWord.charAt(0))){
+                    var pigLatinWord = newWord.concat("ay");
+                    pigLatinArr.push(pigLatinWord);
+                } else {
+                    var secondLetter = newWord.slice(0,1);
+                    var restOfnewWord = newWord.slice(1,newWord.length);
+
+		            //put it at the end of the word
+                    var newNewWord = restOfnewWord+secondLetter;
+                    var pigLatinWord = newNewWord.concat("ay");
+                    pigLatinArr.push(pigLatinWord);
+                }
+            }
+        var pigLatinSentence = pigLatinArr.join(" ");
+        $(".output").append("<p>"+pigLatinSentence+"</p>");
+        });
     }
 
 
