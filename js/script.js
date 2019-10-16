bdf96af74730364f4d0f77e468ee1ef5fd6be931
Anquilis <realanquilis@gmail.com>
2019-10-16 01:52:17 +0000

    Cleaned JS functions

--- js/script.js
+++ js/script.js
@@ -1,40 +1,52 @@
 $( "document" ).ready(function() {
-    var vowels= ["a","e","i","o","u","A","E","I","O","U"];
+    var vowels= ["a","e","i","o","u"];
     var pigLatinArr = [];
     function pigLatinizer(){
+        //takes the input and put it all in lowercase
         var input = $(".input").val();
+        input = input.toLowerCase();
+        //clears the array
+        pigLatinArr = [];
+        //clears the ouput div
+        $(".output").html("");
+        //splits the input into an array between the spaces
         var inputArr = input.split(" ");
-        inputArr.forEach(function(word){
-            if(vowels.includes(word.charAt(0))){
-                var vowelPigLatin = word.concat("way");
-                pigLatinArr.push(vowelPigLatin);
-            } else{
-                //if it isn’t , take the letter out of the word
-                var firstLetter = word.slice(0,1);
-                var restOfWord = word.slice(1,word.length);
-		        //put it at the end of the word
-                var newWord = restOfWord+firstLetter;
 
-		        //repeat until the first character is a vowel
-                //put "ay" at the end of the word
-                if (vowels.includes(newWord.charAt(0))){
-                    var pigLatinWord = newWord.concat("ay");
-                    pigLatinArr.push(pigLatinWord);
-                } else {
-                    var secondLetter = newWord.slice(0,1);
-                    var restOfnewWord = newWord.slice(1,newWord.length);
+        checkForVowels(inputArr);
 
-		            //put it at the end of the word
-                    var newNewWord = restOfnewWord+secondLetter;
-                    var pigLatinWord = newNewWord.concat("ay");
-                    pigLatinArr.push(pigLatinWord);
-                }
-            }
         var pigLatinSentence = pigLatinArr.join(" ");
         $(".output").append("<p>"+pigLatinSentence+"</p>");
-        });
     }
 
+    function checkForVowels(inputArr) {
+        inputArr.forEach(function(word){
+                if(vowels.includes(word.charAt(0))){
+                    var vowelPigLatin = word.concat("way");
+                    pigLatinArr.push(vowelPigLatin);
+                } else {
+                    //if it isn’t , take the letter out of the word
+                    var firstLetter = word.slice(0,1);
+                    var restOfWord = word.slice(1,word.length);
+                    //put it at the end of the word
+                    var newWord = restOfWord+firstLetter;
+
+                    //repeat until the first character is a vowel
+                    //put "ay" at the end of the word
+                    if (vowels.includes(newWord.charAt(0))){
+                        var pigLatinWord = newWord.concat("ay");
+                        pigLatinArr.push(pigLatinWord);
+                    } else {
+                        var secondLetter = newWord.slice(0,1);
+                        var restOfnewWord = newWord.slice(1,newWord.length);
+
+                        //put it at the end of the word
+                        var newNewWord = restOfnewWord+secondLetter;
+                        var pigLatinWord = newNewWord.concat("ay");
+                        pigLatinArr.push(pigLatinWord);
+                    }
+                }
+        });
+    }
 
     $("#button").click(function() {
         pigLatinizer();
